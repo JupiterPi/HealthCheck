c94dc4dbe51fb0b3795116ae3bde00f40b68e1ac
JupiterPi <jupiterpi@outlook.de>
2021-04-16 08:20:52 +0200

    argumental help text

--- src/main/java/jupiterpi/healthcheck/Main.java
+++ src/main/java/jupiterpi/healthcheck/Main.java
@@ -5,7 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
+    private final static String HELP_TEXT = "With this application, two devices check each other regularly, and if no answer arrives, they send a Discord message to the system administrator. ";
+
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        if (args[0].equalsIgnoreCase("help")) {
+            System.out.println(HELP_TEXT);
+            System.out.println("");
+            System.out.println("-- Application not started.");
+        } else SpringApplication.run(Main.class, args);
     }
 }
