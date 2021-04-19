155b6b7e2daf006be059ddb72f9d302e9771f38d
JupiterPi <jupiterpi@outlook.de>
2021-04-19 10:43:44 +0200

    argumental help text correction

--- src/main/java/jupiterpi/healthcheck/Main.java
+++ src/main/java/jupiterpi/healthcheck/Main.java
@@ -8,10 +8,13 @@ public class Main {
     private final static String HELP_TEXT = "With this application, two devices check each other regularly, and if no answer arrives, they send a Discord message to the system administrator. ";
 
     public static void main(String[] args) {
-        if (args[0].equalsIgnoreCase("help")) {
+        if (args.length > 0) {
             System.out.println(HELP_TEXT);
             System.out.println("");
             System.out.println("-- Application not started.");
-        } else SpringApplication.run(Main.class, args);
+            System.exit(1);
+        } else {
+            SpringApplication.run(Main.class, args);
+        }
     }
 }
